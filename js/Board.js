9f80bd1357692340b088418a71a0209f5b331752
Hubert Łepski <hubertlepsky@gmail.com>
2019-07-14 01:09:17 +0200

    Add all files

---
+++ js/Board.js
@@ -0,0 +1,37 @@
+var board = {
+    name: 'Tablica Kanban',
+    addColumn: function (column) {
+        this.element.appendChild(column.element);
+        initSortable(column.id); //About this feature we will tell later
+    },
+    element: document.querySelector('#board .column-container')
+};
+
+document.querySelector('#board .create-column').addEventListener('click', function () {
+    var name = prompt('Wprowadź nazwę kolumny!');
+    var data = new FormData();
+
+    data.append('name', name);
+
+    fetch(prefix + baseUrl + '/column', {
+            method: 'POST',
+            headers: myHeaders,
+            body: data,
+        })
+        .then(function (resp) {
+            return resp.json();
+        })
+        .then(function (resp) {
+            var column = new Column(resp.id, name);
+            board.addColumn(column);
+        });
+
+});
+
+function initSortable(id) {
+    var el = document.getElementById(id);
+    var sortable = Sortable.create(el, {
+        group: 'kanban',
+        sort: true
+    });
+}
